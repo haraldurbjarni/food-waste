a9085ddf363140d726e41adcacefab65ff368356
haraldurbjarni <haraldurbjarni@gmail.com>
2021-04-21 00:42:29 +0000

    work on some stuff

--- foodwaste_backend/app.py
+++ foodwaste_backend/app.py
@@ -16,15 +16,13 @@ import numpy as np
 import os
 import shutil
 import io
+import pickle
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # start flask
 app = Flask(__name__)
 hashmap = {}
-#app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-#app.config['SESSION_TYPE'] =  'filesystem'
-#Session(app)
 CORS(app, supports_credentials=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -37,20 +35,9 @@ def upload_file():
 
         f = request.files['file']
         random_string = os.urandom(10).hex() 
-        #ml_data = genfromtxt(f, delimiter=',')
-        #ml_data = ml_data[1:,1:]
-        #pandas_data = pd.read_csv(f, delimiter=',')
-
-        #ml_data = genfromtxt(f, delimiter=',')
-        #ml_data = ml_data[1:,1:]
-        #print(pandas_data)
-        #print(ml_data)
-        #session['ml_data'] = ml_data
         hashmap[random_string] = f
         f.save(f'./files/{random_string}.csv')
         print('Data key set',random_string,':',f)
-        #session['pandas_data'] = pandas_data
-        #print(session['ml_data'])
         return jsonify({'key': random_string}), 200, {'ContentType':'application/json'} 
 
 @app.route('/api/upload_prices',  methods=['POST'])
@@ -78,19 +65,9 @@ def test():
 @cross_origin(supports_credentials=True)
 def train_model():
     #getting the data from the requests
-    print('request')
     data_key = request.args.get('dataKey')
     price_key = request.args.get('priceKey')
     profit_margin = request.args.get('profitMargin')
-    #data_file = open(hashmap[data_key])
-    #price_file = open(hashmap[price_key])
-
-
-    #pd_data =  pd.read_csv(data_file, delimiter=',')
-
-    #print('data file:', data_file)
-    #print('price_file:', price_file)
-    print('profit margin:', profit_margin)
 
     with open(f'./files/{data_key}.csv','rb') as d:
         pd_data =  pd.read_csv(d, delimiter=',',encoding='utf-8')
@@ -103,9 +80,6 @@ def train_model():
         price_list = pd.read_csv(p, delimiter=',',encoding='utf-8')
         price_dict = dict(sorted(price_list.values.tolist()))
         p.close()
-    print(ml_data)
-    print(price_dict)
-    print(pd_data)
 
     price_list = session.get('price_list')
     cols = list(pd_data.columns)[1:]
@@ -124,24 +98,24 @@ def train_model():
         y_train = y[:y.shape[0]-leave_out_number-1]
         X_val = X[X.shape[0]-1]
         X_val = np.expand_dims(X_val, axis=0)
-        print(X_val.shape)
-        print(X_train.shape)
+
         y_val = y[y.shape[0]-1]
         model.fit(X_train, y_train, epochs=200, verbose=0)
         y_pred = model.predict(X_val)
         y_pred = np.floor(y_pred[0])
-        print('y_pred', y_pred[0])
-        print('y_val', y_val)
-        prediction_array[i] = y_pred
+
+        prediction_array[i] = y_pred[0]
         actual_value_array[i] = y_val
-        print(i)
+
     prediction_array = np.array([0 if i<0 else i for i in list(prediction_array)])
     model_dict = {}
     total_sales_profit = 0
     total_capital_wasted = 0
     total_capital_misseed_out_on = 0
     for i in range(prediction_array.shape[0]):
-        price = price_dict[cols[i]]*profit_margin
+        profit_margin = float(profit_margin)
+        price = (price_dict[cols[i]])*profit_margin
+
         capital_wasted = 0
         capital_missed_out_on = 0
         if actual_value_array[i] > prediction_array[i]:
@@ -149,7 +123,7 @@ def train_model():
             capital_missed_out_on = (actual_value_array[i]-prediction_array[i])*price
         elif actual_value_array[i] < prediction_array[i]:
             sales_profit = actual_value_array[i]*price
-            capital_wasted = (prediction_array[i] - actual_value_array[i])
+            capital_wasted = (prediction_array[i] - actual_value_array[i])*(price_dict[cols[i]]*(1-profit_margin))
         else:
             sales_profit = actual_value_array[i]*price
         total_sales_profit+=sales_profit
@@ -157,12 +131,60 @@ def train_model():
         total_capital_wasted+=capital_wasted
         model_dict[cols[i]] = {
             'Predicted value': prediction_array[i],
-            'Actual value': actual_value_array[i], 
+            'Actual value': actual_value_array[i],
+            'Price': price_dict[cols[i]],
             'Sales profit': sales_profit, 
             'Capital missed out on': capital_missed_out_on, 
-            'Capital_wasted': capital_wasted
+            'Capital wasted': capital_wasted
         }
-    return jsonify({model_dict, total_sales_profit, total_capital_misseed_out_on, total_capital_wasted})
+    model.save(f'./models/{data_key}')
+    return jsonify(model_dict, {'Total sales profit':total_sales_profit,
+                                'Total capital missed out on': total_capital_misseed_out_on,
+                                 'Total capital wasted': total_capital_wasted}
+                                 )
+
+
+@app.route('/api/make_prediction',  methods=['GET'])
+@cross_origin(supports_credentials=True)
+def make_prediction():
+    data_key = request.args.get('dataKey')
+    with open(f'./files/{data_key}.csv','r') as f:
+        ml_data = genfromtxt(f, delimiter=',')
+        ml_data = ml_data[1:,1:]
+        f.close()
+    with open(f'./files/{data_key}.csv','rb') as d:
+        pd_data =  pd.read_csv(d, delimiter=',',encoding='utf-8')
+        d.close()
+    cols = list(pd_data.columns)[1:]
+    loaded_model = tf.keras.models.load_model(f'./models/{data_key}')
+    prediction_array = np.zeros(shape=(len(cols)))
+    n_steps = 14
+    n_features = 1
+    output_window = 7
+    leave_out_number = 7
+    for i,item in enumerate(cols):
+        data = ml_data[:,i]
+        X, y = split_sequence_sum(data,output_window, n_steps)
+        X_val = data[-14:]
+        X_val = np.expand_dims(X_val, axis=0)
+        X_val = X_val.reshape(X_val.shape[0], X_val.shape[1], n_features)
+
+        y_pred = loaded_model.predict(X_val)
+        y_pred = np.floor(y_pred[0])
+
+        prediction_array[i] = y_pred[0]
+
+    prediction_array = np.array([0 if i<0 else i for i in list(prediction_array)])
+    model_dict = {}
+    for i in range(prediction_array.shape[0]):
+        model_dict[cols[i]] = {
+            'Predicted value': prediction_array[i],
+        }
+    return jsonify(model_dict)
+
+
+
+
 
 def create_model():
     #Model 1: LSTM
@@ -170,12 +192,12 @@ def create_model():
     n_steps = 14
     n_features = 1
     output_window = 7
-    model_2.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(n_steps, n_features)))
-    model_2.add(LSTM(50, activation='relu'))
+    model_2.add(LSTM(20, activation='relu', input_shape=(n_steps, n_features)))
+    #model_2.add(LSTM(10, activation='relu', return_sequences=True, input_shape=(n_steps, n_features)))
+    #model_2.add(LSTM(10, activation='relu'))
     model_2.add(Dense(1))
     model_2.compile(optimizer='adam', loss='mse')
     return model_2
-
 
 def split_sequence_sum(sequence, output_window, n_steps):
     X, y = list(), list()
